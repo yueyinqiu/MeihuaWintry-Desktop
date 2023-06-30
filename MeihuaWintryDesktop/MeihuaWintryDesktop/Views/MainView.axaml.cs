c15b85c76dba415e5d280f8bf1d7159762a66d62
yueyinqiu <lujiaqi2003@126.com>
2023-06-30 21:52:02 +0800

    wid

---
+++ MeihuaWintryDesktop/MeihuaWintryDesktop/Views/MainView.axaml.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia.Controls;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MeihuaWintryDesktop;
+
+public partial class MainWindow : Window
+{
+    bool isCloseRequired = false;
+    public MainWindow()
+    {
+        this.InitializeComponent();
+
+        this.Closing += async (_, e) => {
+            if (isCloseRequired)
+                return;
+            
+            e.Cancel = true;
+            if (await this.CheckSavedAndConfirmContinuing())
+            {
+                isCloseRequired = true;
+                this.Close();
+            }
+        };
+    }
+
+    private async Task<bool> CheckSavedAndConfirmContinuing()
+    {
+        return true;
+
+        /*
+        if (caseDisplayControl.IsSaved)
+            return true;
+
+        var window = new MessageWindow();
+        return await window.ShowDialog(
+            this, "占例未保存", "当前占例还没有保存，确定要继续么？", "继续", "取消");
+        */
+    }
+}
